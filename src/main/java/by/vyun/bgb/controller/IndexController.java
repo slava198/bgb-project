51992e6ecd17890f831d22896abf9c69fb8eb443
vyun_slava <slava198@tut.by>
2020-05-07 17:55:20 +0300

    add email service

--- src/main/java/by/vyun/bgb/controller/IndexController.java
+++ src/main/java/by/vyun/bgb/controller/IndexController.java
@@ -1,6 +1,7 @@
 package by.vyun.bgb.controller;
 
 import by.vyun.bgb.service.BoardGameService;
+import by.vyun.bgb.service.EmailService;
 import by.vyun.bgb.service.UserService;
 import by.vyun.bgb.entity.User;
 import by.vyun.bgb.service.MeetingService;
@@ -18,12 +19,15 @@ public class IndexController {
     UserService userService;
     BoardGameService gameService;
     MeetingService meetingService;
+    EmailService emailService;
 
     @GetMapping("/")
     public String index(Model model) {
+
+        emailService.sendSimpleMessage("slava198@tut.by", "test", "Your confirmation code is: 135135");
+
         model.addAttribute("games", gameService.getAllGames());
         model.addAttribute("meetings", meetingService.getAllMeetings());
-        return "index";
     }
 
     @PostMapping("/account")
